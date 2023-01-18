d5ab5d27d0a7e3fde2acd27d09d961d2a309ee64
Ömer Faruk BARAN <of123baran@outlook.com>
2023-01-19 02:56:54 +0300

    Added blog route

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from Controllers import User,Authentication
+from Controllers import User,Authentication,blog
 
 app = FastAPI()
 
@@ -9,6 +9,7 @@ app = FastAPI()
 allowed_origins = ["*"]
 app.add_middleware(CORSMiddleware, allow_origins=allowed_origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 app.include_router(User.router, prefix="/user", tags=["user"])
+app.include_router(blog.router, prefix="/blog", tags=["blog"])
 app.include_router(Authentication.router, prefix="/auth", tags=["auth"])
 
 
