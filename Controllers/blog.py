1f5c6ab58efa1cd9f7bab8add15bb2cb7f5c5c6a
Ömer Faruk BARAN <of123baran@outlook.com>
2023-01-20 02:33:21 +0300

    Added get by author and get by category

--- Controllers/blog.py
+++ Controllers/blog.py
@@ -31,7 +31,7 @@ async def get_from_search(search:str,start:int = None,limit:int = None):
     blogs = await Blog.search(search,start,limit)
     for blog in blogs:
         blog.category = await Category.find_where(id=blog.category)
-        blog.user = await User.find_where(id=blog.user_id)
+        blog.user = await User.find_where(id=blog.user)
         blog.user.password = None
     if blogs == None:
         raise HTTPException(status_code=404, detail="Blog not found")
